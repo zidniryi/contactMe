87577927910ee743a73a688fe69caa91d294a495
zidniryi <zidnirydwan@gmail.com>
2021-05-01 22:14:27 +0700

    feat(src): finish validation helpes form
    finish validation helpes form input

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -3,3 +3,18 @@ export const regex = {
   age: /^\d{0,3}$/,
   urlPhoto: /^((https?|ftp):)?\/\/.*(jpeg|jpg|png|gif|bmp)$/,
 };
+
+export const validationForm = (firstName, lastName, age, photo) => {
+  // Validation
+  const validationForm =
+    regex.name.test(firstName) &&
+    firstName &&
+    regex.name.test(lastName) &&
+    lastName &&
+    regex.name.test(age) &&
+    age &&
+    regex.urlPhoto.test(photo) &&
+    photo
+      ? false
+      : true;
+};
--- src/views/addScreen/index.js
+++ src/views/addScreen/index.js
@@ -1,18 +1,23 @@
 import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Text, ScrollView} from 'react-native';
+import {
+  responsiveFontSize,
+  responsiveHeight,
+  responsiveWidth,
+} from 'react-native-responsive-dimensions';
 import {postContact} from '../../api/contact';
 
 import Button from '../../components/Button';
 import HeaderApp from '../../components/HeaderApp';
 import TextInputApp from '../../components/TextInputApp';
 import Title from '../../components/Title';
+import {regex, validationForm} from '../../helpers';
 
 export default function AddScreen() {
   const [firstName, setfirstName] = useState('');
   const [lastName, setlastName] = useState('');
   const [age, setage] = useState('');
   const [photo, setphoto] = useState('');
-  const [isDisable, setisDisable] = useState(true);
 
   const postContactAPI = async () => {
     const dataContact = {
@@ -36,32 +41,57 @@ export default function AddScreen() {
     <View style={styles.viewContainer}>
       <HeaderApp title={'ADD CONTACT'} isBack />
       <Title title="Add New Contact" />
-      <View>
-        <TextInputApp
-          placeholder="First Name"
-          value={firstName}
-          onChangeText={value => setfirstName(value)}
-        />
-        <TextInputApp
-          placeholder="Last Name"
-          value={lastName}
-          onChangeText={value => setlastName(value)}
-        />
-        <TextInputApp
-          placeholder="Age"
-          value={age}
-          onChangeText={value => setage(value)}
-        />
-        <TextInputApp
-          placeholder="URL"
-          value={photo}
-          onChangeText={value => setphoto(value)}
-        />
-      </View>
+      <ScrollView>
+        <View>
+          <TextInputApp
+            placeholder="First Name"
+            value={firstName}
+            onChangeText={value => setfirstName(value)}
+          />
+          <Text style={styles.textError}>
+            {regex.name.test(firstName) || !firstName
+              ? ''
+              : 'Input with First Name'}
+          </Text>
+          <TextInputApp
+            placeholder="Last Name"
+            value={lastName}
+            onChangeText={value => setlastName(value)}
+          />
+          <Text style={styles.textError}>
+            {regex.name.test(lastName) || !lastName
+              ? ''
+              : 'Input with First Last Name'}
+          </Text>
+          <TextInputApp
+            placeholder="Age"
+            value={age}
+            onChangeText={value => setage(value)}
+          />
+          <Text style={styles.textError}>
+            {regex.name.test(lastName) || !lastName
+              ? ''
+              : 'Input with Age Number'}
+          </Text>
+          <TextInputApp
+            placeholder="URL"
+            value={photo}
+            onChangeText={value => setphoto(value)}
+          />
 
-      <View>
-        <Button textButton="SAVE" onPress={postContactAPI} isDisable />
-      </View>
+          <Text style={styles.textError}>
+            {regex.urlPhoto.test(photo) || !photo ? '' : 'Input with URL image'}
+          </Text>
+        </View>
+
+        <View>
+          <Button
+            textButton="SAVE"
+            onPress={postContactAPI}
+            isDisable={validationForm()}
+          />
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -70,5 +100,11 @@ const styles = StyleSheet.create({
   viewContainer: {
     flex: 1,
     backgroundColor: '#FFFFFF',
+  },
+  textError: {
+    fontSize: responsiveFontSize(1.5),
+    color: 'red',
+    marginLeft: responsiveWidth(5.33),
+    marginTop: responsiveHeight(-1.33),
   },
 });
